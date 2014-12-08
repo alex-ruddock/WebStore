ba3de462176103de200c7026a0b6fb5fbd004318
Alex <amr_sauron@msn.com>
2014-12-07 21:56:38 -0500

    fff

--- Purchase.py
+++ Purchase.py
@@ -10,7 +10,7 @@ def main():
 	form = cgi.FieldStorage()
 	inventory = []
 	content = ""
-	log = False
+	log = True
 	
 	with open ('LoggedIn.csv', 'r') as logd:
 		userz = []
@@ -58,8 +58,8 @@ def main():
 		else:
 			wave = 0	
 
-		with open ('Inventory.csv', 'r') as fp:	
-			for line in fp:
+		with open ('Inventory.csv', 'r') as f:	
+			for line in f:
 				line = (line.split(","))
 				inventory.append(int(line[1]))
 
